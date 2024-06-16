b886f4eb3e2dd4989b9211c16a535feeaff06f37
unknown <1142281477@qq.com>
2024-06-16 22:14:27 +0800

    注释

--- plunder.py
+++ plunder.py
@@ -1,4 +1,3 @@
-from ast import If
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -6,9 +5,9 @@ import time
 # 设置Chrome浏览器的选项
 options = webdriver.ChromeOptions()
 options.add_argument('--no-sandbox')
-options.add_argument('--headless')  # 如果不需要浏览器界面，可启用无头模式
+# options.add_argument('--headless')  # 如果不需要浏览器界面，可启用无头模式
 options.add_argument("--window-size=1920,1080")
-options.add_argument('--disable-gpu')  # 适用于无头模式
+# options.add_argument('--disable-gpu')  # 适用于无头模式
 
 # 初始化webdriver
 driver = webdriver.Chrome(options=options)
@@ -16,6 +15,7 @@ driver = webdriver.Chrome(options=options)
 # 打开指定的URL
 url = 'https://wap.showstart.com/pages/order/activity/confirm/confirm?sequence=231189&ticketId=f2543a9ee746e94a481b30956a0a4544&ticketNum=1'
 driver.get(url)
+print('打开页面')
 # 等待10s加载完成
 time.sleep(10)
 
@@ -25,7 +25,6 @@ key = 'userInfo'
 strs = '{"type":"object","data":{"st_flpv":"70qikoz9l1wawnbulxw77jbkehd9mktf","userId":9256095,"userType":1,"tuType":0,"userName":"蚍蜉渡海_","avatar":"https://s2.showstart.com/img/2024/0412/09/30/d5398f9b27374011b06dd4b77baa2b88_200_200_13964.0x0.png","loginTime":1718087319965,"sign":"cba36e01b753e18aeae8a723154a1e26","ticketWalletServiceStatus":2,"tel":"15539502921","expireTime":1720679319967,"isChoose":1,"areaCode":"86_CN","uniqueCode":"56cf13c5c37d4fa68c0e4f696e4ed592","imAppId":1400331755,"isRealName":2,"isAdmin":0,"userLevel":13,"memberExpireTime":0,"idtoken":"87a79e22fb8227886667f6cb525123ae","id":9256095,"name":"蚍蜉渡海_","isCollect":0,"creditLevel":0,"creditLevelExp":[20,131,420,1634,4257]}}'
 driver.execute_script(f"window.localStorage.setItem('{key}', '{strs}');")
 driver.get(url)
-
 # 打印userInfo 确认成功
 script = "return window.localStorage.getItem('userInfo');"
 value = driver.execute_script(script)
@@ -41,9 +40,11 @@ time.sleep(1)
 # 点击确定按钮
 confirm_button = driver.find_element(By.XPATH, '//uni-view[contains(text(), "确定")]')
 confirm_button.click()
+print('选择观演人')
 
 time.sleep(10)
 pay_btn = driver.find_elements(By.CLASS_NAME, "payBtn")[0]
+print('获取按钮')
 
 # 定义确认方法
 def start():
